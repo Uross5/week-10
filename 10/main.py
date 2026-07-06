9444c327bddf3e449f2f4b15f305a3da8fdb307a
Uros Cukic <uros.cukic@gmail.com>
2026-07-05 20:38:30 -0400

    Finished main python and data.json

--- 10/main.py
+++ 10/main.py
@@ -1,1 +1,14 @@
-#Test
+import json
+from textwrap import indent
+
+with open("data.json", "r") as file:
+    data=json.load(file)
+    data.append({
+        "name":"Luke Thompson",
+        "age":25,
+        "height":170,
+        "gender":"male"
+    })
+
+    with open("data.json","w") as file:
+        json.dump(data,file,indent=4)
